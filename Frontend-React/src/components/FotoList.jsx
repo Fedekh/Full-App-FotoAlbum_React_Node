db60735cb9dca0f5459e0dfced028458c751c023
Federico Ceteroni <f.ceteroni.ext@teamsystem.com>
2023-12-20 17:36:12 +0100

    ds

--- Frontend-React/src/components/FotoList.jsx
+++ Frontend-React/src/components/FotoList.jsx
@@ -7,8 +7,8 @@ export default function FotoList() {
 
   useEffect(() => {
     fetchDataAll();
-
-  }, [fetchDataAll]);
+  }, []);
+  
 
   return (
     <div>
--- Frontend-React/src/pages/FotoShow.jsx
+++ Frontend-React/src/pages/FotoShow.jsx
@@ -12,7 +12,7 @@ export default function FotoShow() {
     useEffect(() => {
         fetchData(idNumerico);     
         
-    }, [fetchData]);
+    }, []);
     
 
     return (
