59708f048359e9f5a8e23d6cfae3286cc6622ae5
Federico Ceteroni <f.ceteroni.ext@teamsystem.com>
2024-03-04 11:56:55 +0100

    change

--- Frontend-React/src/pages/FotoBlog.jsx
+++
@@ -1,9 +0,0 @@
-import { BlogProvider } from '../contexts/BlogContext';
-import FotoList from '../components/FotoList';
-
-
-export default function Fotoblog() {
-  return (
-      <FotoList />
-  );
-};
